4d2971b6a87f67640665abcb50d9ffc94805c36d
Zejin Shi <shizejin94@gmail.com>
2017-12-02 23:20:07 +0900

    Implement ranking game

--- quantecon/game_theory/game_generators/tests/test_bimatrix_generators.py
+++ quantecon/game_theory/game_generators/tests/test_bimatrix_generators.py
@@ -7,7 +7,7 @@ from numpy.testing import assert_array_equal
 from nose.tools import eq_, ok_
 from quantecon.gridtools import num_compositions
 
-from quantecon.game_theory import blotto_game, sgc_game
+from quantecon.game_theory import blotto_game, ranking_game, sgc_game
 
 
 class TestBlottoGame:
@@ -31,6 +31,34 @@ class TestBlottoGame:
         rho = -0.5
         g0 = blotto_game(h, T, rho, random_state=seed)
         g1 = blotto_game(h, T, rho, random_state=seed)
+        for i in range(self.g.N):
+            assert_array_equal(g0.players[i].payoff_array,
+                               g1.players[i].payoff_array)
+
+
+class TestRankingGame:
+    def setUp(self):
+        self.n = 100
+        self.g = ranking_game(self.n)
+        self.payoff_arrays = [player.payoff_array for player in self.g.players]
+
+    def test_size(self):
+        eq_(self.g.nums_actions, (self.n, self.n))
+
+    def test_weakly_decreasing_row_wise_payoffs(self):
+        for payoff_array in self.payoff_arrays:
+            ok_((payoff_array[:, 1:-1] >= payoff_array[:, 2:]).all())
+
+    def test_elements_first_row(self):
+        eq_(self.payoff_arrays[0][0, 0] + self.payoff_arrays[1][0, 0], 1.)
+        for payoff_array in self.payoff_arrays:
+            ok_(np.isin(payoff_array[0, :], [0, 1, 0.5]).all())
+
+    def test_seed(self):
+        seed = 0
+        n = 100
+        g0 = ranking_game(n, random_state=seed)
+        g1 = ranking_game(n, random_state=seed)
         for i in range(self.g.N):
             assert_array_equal(g0.players[i].payoff_array,
                                g1.players[i].payoff_array)
