011a7bc5779f4f5ce91082d6919d16b5e7168821
QBatista <quentin.batista@mail.mcgill.ca>
2020-05-11 13:01:24 +0900

    temp

--- quantecon/optimize/okumura/tuning_LP_solver.py
+++ quantecon/optimize/okumura/tuning_LP_solver.py
@@ -15,25 +15,39 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
+%%time
+
+# import
+from quantecon.optimize import linprog_simplex
+#from quantecon.gridtools import cartesian
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+from collections import namedtuple
+from itertools import product
+import glob
+from multiprocessing import Pool, cpu_count
+
+
 def main(param_problem_pair):
     param = param_problem_pair[0]
-    problem_name = param_problem_pair[1]
-    problem = problem = np.load(problem_name)
+    problem_nb = param_problem_pair[1]
 
     piv_options = PivOptions(param[0], param[1], param[2])
 
-    problem = np.load(problem_name)
-    c = - problem['c']
-    A_eq = problem['A_eq']
-    A_ub = problem['A_ub']
-    b_eq = problem['b_eq']
-    b_ub = problem['b_ub']
-    desired_fun = problem['obj']
+    c = cs[problem_nb]
+    A_eq = A_eqs[problem_nb]
+    A_ub = A_ubs[problem_nb]
+    b_eq = b_eqs[problem_nb]
+    b_ub = b_ubs[problem_nb]
+    desired_fun = desired_funs[problem_nb]
     res = linprog_simplex(c, A_eq=A_eq, b_eq=b_eq, A_ub=A_ub, b_ub=b_ub,
-                          piv_options=piv_options)
-    problem_name = problem_name[len('./linprog_benchmark_files/'):-4]
+                          piv_options=piv_options,
+                          max_iter=10_000)
 
-    result_dict = {'param': str(param), 'problem': problem_name,
+    result_dict = {'param': str(param), 'problem': problem_nb,
                    'res.fun': res.fun, 'desired_fun': -desired_fun,
                    'success?': np.isclose(res.fun, -desired_fun),
                    'status': res.status}
@@ -58,24 +72,18 @@ if __name__ == '__main__':
     for name in remove_list:
         problem_list.remove(data_dir + name + '.npz')
 
+    # Below, we work on a small problem set
     PivOptions = namedtuple(
         'PivOptions', ['fea_tol', 'tol_piv', 'tol_ratio_diff']
     )
 
-    fea_tol_list = [1e-4, 1e-5, 1e-6, 1e-7, 1e-8]
-    tol_piv_list = [1e-8, 1e-9, 1e-10, 1e-11, 1e-12]
-    tol_ratio_diff_list = [1e-13, 1e-14, 1e-15, 1e-16, 1e-17]
-
-    # If you want to test with a small parameter sets, comment out this:
-    # fea_tol_list = [1e-5, 1e-6]
-    # tol_piv_list = [1e-10, 1e-11]
-    # tol_ratio_diff_list = [1e-15, 1e-16]
-
+    fea_tol_list = [1e-5, 5e-6, 1e-6, 5e-7, 1e-7]
+    tol_piv_list = [1e-7, 5e-8, 1e-8, 5e-9, 1e-9]
+    tol_ratio_diff_list = [1e-15, 5e-15, 1e-14, 5e-14, 1e-13]
     param_set = product(fea_tol_list, tol_piv_list, tol_ratio_diff_list)
 
-    # If you want to test with a small problem sets, comment out this:
-    # problem_list = ['./linprog_benchmark_files/AFIRO.npz',
-    #                 './linprog_benchmark_files/AGG.npz']
+#     problem_list = ['./linprog_benchmark_files/AFIRO.npz',
+#                     './linprog_benchmark_files/AGG.npz']
 
     num_problem = len(problem_list)
     num_param =\
@@ -88,12 +96,21 @@ if __name__ == '__main__':
     df.columns = column_list
     row_index = 0
 
-    param_problem_list = product(param_set, problem_list)
+    param_problem_list = product(param_set, range(num_problem))
+
+    problems = [np.load(problem) for problem in problem_list]
+
+    cs = tuple(-problem['c'] for problem in problems)
+    A_eqs = tuple(problem['A_eq'] for problem in problems)
+    A_ubs = tuple(problem['A_ub'] for problem in problems)
+    b_eqs = tuple(problem['b_eq'] for problem in problems)
+    b_ubs = tuple(problem['b_ub'] for problem in problems)
+    desired_funs = tuple(problem['obj'] for problem in problems)
 
     # parallelization
-    n_cores = cpu_count()
-    p = Pool(n_cores)
-    result_dicts = p.map(main, param_problem_list)
+    n_cores = cpu_count() // 2
+    with Pool(n_cores, maxtasksperchild=1000) as p:
+        result_dicts = p.map(main, param_problem_list)
 
     # convertion to pandas dataframe
     for row_index, result_dict in enumerate(result_dicts):
@@ -104,7 +121,7 @@ if __name__ == '__main__':
         df['success?'][row_index] = result_dict['success?']
         df['status'][row_index] = result_dict['status']
 
-    # print(df)
+    print(df)
     df.to_csv('results.csv')
 
     # visualization
@@ -116,4 +133,4 @@ if __name__ == '__main__':
 
     plt.spy(result_matrix, markersize=3)
     plt.savefig('result_table.png')
-    # plt.show()
+    plt.show()
